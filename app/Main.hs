22566ce5e7ed0201576d3e4635b58e0c36a01fc7
DashaAntonova <Miabelika@gmail.com>
2017-02-14 14:41:34 +0500

    Cassava

--- app/Main.hs
+++ app/Main.hs
@@ -1,60 +1,60 @@
-module Main where
+{-# LANGUAGE ScopedTypeVariables, DeriveGeneric #-}
 
-import Data.Foldable
-import Data.Strings
-import Graphics.UI.Gtk
-import Lib
-import System.Environment
+import qualified Data.ByteString.Lazy as BL
+import Data.Csv
+import qualified Data.Vector as V
+import GHC.Generics
+import Data.Foldable (traverse_)
+import Data.List
+import Data.Time    --использую Time, а не dateTime. Ишус про импорты
+import Data.Maybe
+import Data.Tuple.HT
 
+{-Помню, когда-то ты говорил, что нужно юзать не String, а Text. Здесь не стала так делать,
+потому что в функции parseTimeM как раз String, а не Text. Или нужно как-то по-другому поступить?-}
+data Data_csv = Data_csv String Int deriving (Generic, Show)
 
+data Pomodoros = Pomodoros {
+    day :: Day,
+    sum :: Int 
+} deriving (Show)
 
+instance FromRecord Data_csv
+instance ToRecord Data_csv
 
-import qualified Data.DateTime as DT
-import qualified Data.Text as T
-import qualified Data.Text.IO as T
-import qualified Data.Text.Internal as T
+valuesToList :: Data_csv -> Pomodoros
+valuesToList (Data_csv a b) = Pomodoros dayFromCsv b
+                                where dayFromCsv = localDay localTime
+                                      localTime = zonedTimeToLocalTime (fromJust zonedTime)
+                                      zonedTime = (parseTimeM True defaultTimeLocale iso8601 a :: Maybe ZonedTime)
+                                    
+{-В этой функции не знала, как быть с Left err. В итоге сделала очень криво... В случае ошибки
+возвращаю какое-то странное значение, просто для того, чтобы вернуть хоть что-то, соответствующее типу. 
+Не придумала, как тут быть. Может быть, нужно использовать Maybe?-}
+listFromCsv :: BL.ByteString -> [Data_csv]
+listFromCsv csvData = 
+    case decode NoHeader csvData of
+        Left err -> [(Data_csv err 0)]
+        Right v -> V.toList v
 
-func :: T.Text -> (T.Text, T.Text, DT.DateTime)
-func str = (zFilter !! 0, zFilter !! 1, DT.fromSeconds (read (T.unpack (zFilter !! 0))) )
-            where zFilter = filter (\x -> if x /= T.pack (",") then True else False) z
-                  z = T.groupBy (\x y -> if (x /= ',' && y /= ',') then True else False) str
+sumPomodoros :: [Pomodoros] -> Pomodoros
+sumPomodoros array = Pomodoros (day (head array)) sumArray
+                where sumArray = foldl (\x (Pomodoros a b) -> x+b) 0 array
 
-doArray :: [T.Text] -> [(T.Text, T.Text, DT.DateTime)]
-doArray a = map func a
+equalPomodoros :: Pomodoros -> Pomodoros -> Bool
+equalPomodoros (Pomodoros x y) (Pomodoros a b) = if x == a then True else False
 
-unpack' :: (T.Text, T.Text, DT.DateTime) -> (String, String, String, (Integer, Int, Int, Int, Int, Int))
-unpack' (x, y, z) = (T.unpack x, T.unpack y, show z, DT.toGregorian z)
+iso8601 :: String
+iso8601 = iso8601DateFormat $ Just "%H:%M:%S%Q%z"
 
+main :: IO ()
 main = do
-    contents <- T.readFile "pomodoros.csv"
-    let todoTasks = T.lines contents
-        res = doArray todoTasks
-        x = map unpack' res
-
-    {-initGUI
-    window <- windowNew
-    button <- buttonNew
-    set window [windowDefaultWidth := 200, windowDefaultHeight := 200,
-        containerChild := button, containerBorderWidth := 30]
-    onClicked button (hello button)
-    onDestroy window mainQuit
-    widgetShowAll window
-    mainGUI-}
-
-    traverse_ print x
+    csvData <- BL.readFile "pomodoros.csv"
+    let list = map valuesToList (listFromCsv csvData)
+    --traverse_ print list
+    
+    --вот, получила список дней с количеством секунд
+    let groupList = map sumPomodoros (groupBy equalPomodoros list)
+    traverse_ print groupList
+            
     putStrLn "End"
-
-{-main = do
-    getArgsAndInitialize
-    createAWindow "Red Window"
-    mainLoop
-
-createAWindow windowName = do
-    createWindow windowName
-    displayCallback $= display
-
-display = do
-    currentColor $= Color4 0 0 0 1
-    clear [ColorBuffer]
-
-    flush-}
